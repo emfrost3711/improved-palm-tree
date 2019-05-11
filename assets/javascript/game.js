a3f36ad281da392316a5114b86dc3bee5ac6f37c
Emily Frost <emfrost@gmail.com>
2019-05-11 17:53:20 -0600

    functional with reset function created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,79 @@
 //GLOBAL VARIABLES//
-    var wins = 0;
-    var losses = 0;
-    var crystalValue = 0;
+var wins = 0;
+var losses = 0;
+var crystalValue = 0;
+var counter = 0;
+var targetNumber = 0;
+var crystal = []
 
 //START FUNCTION//
-function start (){   
+function reset() {
+    counter = 0;
+    targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
+    $("#currentScore").text(counter);
+    $("#targetNumber").text(targetNumber);
+    crystal = {
+        blue: Math.floor(Math.random() * 13 + 1),
+        green: Math.floor(Math.random() * 13 + 1),
+        red: Math.floor(Math.random() * 13 + 1),
+        purple: Math.floor(Math.random() * 13 + 1),
+    }
 
-var targetNumber = Math.floor(Math.random() * (120-19) +19);
-// console.log(targetNumber + "this is our target number");
-var counter = 0; 
-
-var crystal = {
-    blue: Math.floor(Math.random() * 13 +1),
-    green: Math.floor(Math.random() * 13 +1),
-    red: Math.floor(Math.random() * 13 +1),
-    purple: Math.floor(Math.random() * 13 +1)
-   
-} 
-$("#blue").attr("data-value", crystal.blue)
-$("#green").attr("data-value", crystal.green)
-$("#red").attr("data-value", crystal.red)
-$("#purple").attr("data-value", crystal.purple)
-console.log("purple" + crystal.purple)
-console.log("red" + crystal.red)
-console.log("blue" + crystal.blue)
-console.log("green" + crystal.green)
-$("#targetNumber").text(targetNumber);  
-
-$(".crystalPic").on("click", function () {
-console.log("Crystal clicked!");
-var crystalValue=$(this).attr("data-value");
-console.log(crystalValue);
-
-counter+=parseInt(crystalValue);
-console.log(counter + "counter");
-$("#currentScore").text(counter);
-
-if (counter > targetNumber) {
-    console.log("User loses!");
-    losses++;
-    $("#losses").text(losses);
-    $("#currentScore").click(function(){
-        $("currentScore").empty();
-      });
-
-} else if (counter === targetNumber) {
-    console.log("User wins!");
-    wins++;
-    $("#wins").text(wins);
-} 
-})
 }
 
- //CALL START FUNCTION 
-start ()
+function start() {
+
+   reset ()
+
+    $("#blue").attr("data-value", crystal.blue)
+    $("#green").attr("data-value", crystal.green)
+    $("#red").attr("data-value", crystal.red)
+    $("#purple").attr("data-value", crystal.purple)
+    console.log("purple" + crystal.purple)
+    console.log("red" + crystal.red)
+    console.log("blue" + crystal.blue)
+    console.log("green" + crystal.green)
+
+    $(".crystalPic").on("click", function () {
+        console.log("Crystal clicked!");
+        var crystalValue = $(this).attr("data-value");
+        console.log(crystalValue);
+
+        console.log("counter is " + counter);
+
+        counter += parseInt(crystalValue);
+
+        console.log("counter is " + counter);
+
+        console.log(counter + "counter");
+        $("#currentScore").text(counter);
+
+        if (counter > targetNumber) {
+            console.log("User loses!");
+            //Update game variables
+            losses++;
+            counter = 0;
+            // Update display
+            $("#losses").text(losses);
+            console.log("counter is " + counter);
+            reset();
+
+        } else if (counter === targetNumber) {
+            console.log("User wins!");
+            wins++;
+            $("#wins").text(wins);
+            reset();
+        }
+        console.log("counter is " + counter);
+    })
+}
+
+//CALL START FUNCTION 
+start()
 
 
 //reset/empty function
 
 
+//print out variables to figure how they're getting updated
+//need to link the counter 
